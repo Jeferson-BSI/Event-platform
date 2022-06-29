721f9522ed69404874a161e11f003c48380b4eec
jeferson-bsi <jefersoncou18@gmail.com>
2022-06-28 22:50:02 -0300

    create routes

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { isPast, format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { Link } from 'react-router-dom';
 
 interface LessonProps {
   title: string;
@@ -19,10 +20,10 @@ export function Lesson(props: LessonProps) {
   );
 
   return (
-    <a href="#">
+    <Link className="group" to={`/event/lesson/${props.slug}`}>
       <span className="text-gray-300 ">{availableAtDateFormat}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="flex itens-center gap-2 text-sm text-blue-500 font-medium">
@@ -39,6 +40,6 @@ export function Lesson(props: LessonProps) {
         </header>
         <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,19 +1,18 @@
+import { useParams } from 'react-router-dom';
 import { Header } from '../components/Header';
-import { Lesson } from '../components/Lesson';
 import { SideBar } from '../components/SideBar';
 import { Video } from '../components/Video';
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <Video />
+        {slug ? <Video lessonSlug={slug}/> : <div className="flex flex-1" />}
         <SideBar />
       </main>
-      {/* 
-      <Lesson />
-      <Video /> */}
     </div>
   );
 }
